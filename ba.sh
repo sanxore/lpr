e2b45eafd1b154117f033b22ad0592c4c2337dc9
AymanYac <yac.ayman@hotmail.com>
2016-05-09 12:21:12 +0100

    bash + geeky

---
+++ ba.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install tipa xfonts-tipa
+wget http://mirrors.ctan.org/macros/latex/contrib/fncychap/fncychap.sty
+wget http://mirrors.ctan.org/macros/latex/contrib/multirow/multirow.sty
+wget http://mirrors.ctan.org/fonts/tipa/tipa/sty/tipa.sty
+wget http://tug.ctan.org/fonts/tipa/tipa/sty/t3enc.def
+wget http://www.cs.cmu.edu/afs/cs/misc/tex/common/teTeX-1.0/lib/texmf/tex/latex/misc/soul.sty
+wget http://web.mit.edu/jhawk/mnt/spo/tex/share/texmf/tex/latex/misc/moreverb.sty
